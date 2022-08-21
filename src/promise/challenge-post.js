12aee09f5781244ffcfe399306e6da664a74ccb0
David Mercado Juanes <davejuanes>
2022-08-21 19:50:38 -0400

    METODO POST REGISTRO DE PRODUCTOS EN API CON FETCH PROMISE

---
+++ src/promise/challenge-post.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+const API = 'https://api.escuelajs.co/api/v1';
+
+function postData(urlApi, data) {
+    const response = fetch(urlApi, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response;
+}
+
+const data = {
+    "title": "New Product Curso 12",
+    "price": 270,
+    "description": "A description 270",
+    "categoryId": 1,
+    "images": ["https://placeimg.com/640/480/any"]
+}
+
+postData(`${API}/products`, data)
+    .then(response => response.json())
+    .then(data => console.log(data))
